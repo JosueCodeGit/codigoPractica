d99456ead6f44aa8abbfd11e921c3666d6bbc8ed
JosueCodeGit <josueherreragit@gmail.com>
2023-04-12 13:27:07 -0600

    Modifique el archivo python...

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,10 @@
-print("Hello World...!")
+# Funcion de la calculadora
+def calculadora():
+	opcion = input('''Escoja su opcion:
+
+Escriba 1 para Sumar
+Escriba 2 para Restas
+Escriba 3 para Multiplicar
+
+>>>''')
+	opcion = int(opcion)
